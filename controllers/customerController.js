2155ea067b0fc9411dd6ff6353dc89650f0efa71
Brendan Castillo <bomber8183@gmail.com>
2023-06-11 12:10:07 +0800

    Updated customer APIs to integrate DB

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,45 +1,64 @@
 const Customers = require('../models/customer');
+const db = require('../database/db');
 
-
-// Mini database 
-var list = []; 
 
 // GET /api/customers
 exports.getCustomers = (req, res, next) => {
-    res.status(200).json({
-        customers: list
+    db.query('SELECT * FROM Customer', (err, rows, fields) => {
+        if (!err) {
+            res.status(200).json({
+                customers: rows
+            });
+        } else {
+            res.status(404).json({
+                message: 'Customers not found!'
+            });
+        }
     });
 }
 
 // GET /api/customers/:id
 exports.getCustomer = (req, res, next) => {
     const id = req.params.id;
-    const customer = list.find(customer => customer.id === id);
-    if (!customer) {
-        res.status(404).json({
-            message: 'Customer not found!'
-        });
-    }
-    res.status(200).json({
-        customer: customer
+    db.query('SELECT * FROM Customer WHERE id = ?', [id], (err, rows, fields) => {
+        if (!err) {
+            res.status(200).json({
+                customer: rows[0]
+            });
+        } else {
+            res.status(404).json({
+                message: 'Customer not found!'
+            });
+        }
     });
 }
 
 // POST /api/customers 
 exports.createCustomer = (req, res, next) => {
-    const id = Math.random().toString();
     const meal = req.body.meal;
     const drink = req.body.drink;
     const sides = req.body.sides;
     const total = req.body.total;
-    const date = req.body.date;
+    const date =  new Date().toISOString().split('T')[0];
+    const mquan = req.body.mquan;
+    const squan = req.body.squan;
+    const dquan = req.body.dquan;
 
-    const customer = new Customers(id, meal, drink, sides, total, date);
-    list.push(customer);
 
-    res.status(201).json({
-        message: 'Customer created successfully!',
-        customer: customer
+    const customer = new Customers(meal, drink, sides, total, date, mquan, squan, dquan);
+
+    db.query('INSERT INTO Customer SET ?', [customer], (err, rows, fields) => {
+        if (!err) {
+            res.status(201).json({
+                message: 'Customer created successfully!',
+                customer: customer
+            });
+        } else {
+            res.status(404).json({
+                // Show sql error message
+                message: err.sqlMessage
+            });
+        }
     });
 }
 
@@ -51,35 +70,38 @@ exports.updateCustomer = (req, res, next) => {
     const sides = req.body.sides;
     const total = req.body.total;
     const date = req.body.date;
+    const mquan = req.body.mquan;
+    const squan = req.body.squan;
+    const dquan = req.body.dquan;
 
-    const customerIndex = list.findIndex(customer => customer.id === id);
-    if (customerIndex < 0) {
-        res.status(404).json({
-            message: 'Customer not found!'
-        });
-    }
+    const customer = new Customers(meal, drink, sides, total, date, mquan, squan, dquan);
 
-    list[customerIndex] = new Customers(id, meal, drink, sides, total, date);
-
-    res.status(200).json({
-        message: 'Customer updated successfully!',
-        customer: list[customerIndex]
+    db.query('UPDATE Customer SET ? WHERE id = ?', [customer, id], (err, rows, fields) => {
+        if (!err) {
+            res.status(200).json({
+                message: 'Customer updated successfully!',
+                customer: customer
+            });
+        } else {
+            res.status(404).json({
+                message: 'Customer not found!'
+            });
+        }
     });
 }
 
 // DELETE /api/customers/:id
 exports.deleteCustomer = (req, res, next) => {
     const id = req.params.id;
-    const customerIndex = list.findIndex(customer => customer.id === id);
-    if (customerIndex < 0) {
-        res.status(404).json({
-            message: 'Customer not found!'
-        });
-    }
-
-    list.splice(customerIndex, 1);
-
-    res.status(200).json({
-        message: 'Customer deleted successfully!'
+    db.query('DELETE FROM Customer WHERE id = ?', [id], (err, rows, fields) => {
+        if (!err) {
+            res.status(200).json({
+                message: 'Customer deleted successfully!'
+            });
+        } else {
+            res.status(404).json({
+                message: 'Customer not found!'
+            });
+        }
     });
 }
--- models/customer.js
+++ models/customer.js
@@ -1,11 +1,13 @@
 class customer {
-    constructor(id, meal, drink, sides, total, date) {
-        this.id = id;
-        this.meal = meal;
-        this.drink = drink;
+    constructor(main, sides, drinks, total, date, mquan, squan, dquan) {
+        this.main = main;
         this.sides = sides;
+        this.drinks = drinks;
         this.total = total;
         this.date = date;
+        this.mquan = mquan;
+        this.squan = squan;
+        this.dquan = dquan;
     }    
 }
 
