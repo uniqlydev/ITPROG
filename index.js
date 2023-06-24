bcdede0c4d6bc24f6f62c507b5902a97e374bb1b
Brendan Castillo <bomber8183@gmail.com>
2023-06-24 13:19:39 +0800

    Post req being handled

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const customerRoutes = require('./routes/customerRoutes');
-const customerController = require('./controllers/customerController');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -11,6 +11,8 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 8080;
 
@@ -43,15 +45,20 @@ app.get('/', (req, res) => {
 });
 
 // Handle post from home to order_success
-app.post('/customer', (req, res) => {
-
-    // Send res.status from createCustomer
-    const status = customerController.createCustomer(req, res);
-
-    // If status is 201, redirect to order_success
-    if (status == 201) {
-        res.render(
-            'home/order_success',
-        );
-    }
-});
+app.post('/customer2', (req, res) => {
+    // Access the data sent in the request body
+    const meal = req.body.meal;
+    const mquan = req.body.mquan;
+    const drink = req.body.drink;
+    const dquan = req.body.dquan;
+    const sides = req.body.sides;
+    const squan = req.body.squan;
+    const total = req.body.total;
+  
+    // Do something with the data (e.g., store it, process it, etc.)
+    console.log('Received data:', req.body);
+  
+    // Send a response if needed
+    res.send('Data received successfully.');
+  });
+  
