ef6ccbcc2a78e63e113f2be0d9ffe68fe3ca592d
Brendan Castillo <bomber8183@gmail.com>
2023-06-25 13:10:23 +0800

    ETO NA WASSMER

--- database.sql
+++ database.sql
@@ -1,17 +1,3 @@
--- CREATE TABLE Customer (
---                         id int(10) NOT NULL AUTO_INCREMENT,
---                         main VARCHAR(40) ,
---                         sides VARCHAR(40),
---                         drinks VARCHAR(40),
---                         total DOUBLE NULL,
---                         date DATE,
---                         mquan INT(2) NOT NULL , # Quantity meal
---                         squan INT(2) NOT NULL, # Quantity Sides
---                         dquan INT(3) NOT NULL,  # Quantity drinks
---                         # Setting up Primary key
---                         PRIMARY KEY (id)
--- );
-
 CREATE TABLE Customer (
                         id int(10) NOT NULL AUTO_INCREMENT,
                         main VARCHAR(40) ,
