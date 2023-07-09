45249a137cb33c2867da5f342e697c772bd92e7b
Mujina <mocpayao15@gmail.com>
2023-07-09 19:18:07 +0800

    finished login page w css

---
+++ views/login/loginPage.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>Login Page</title>
+    <link rel="stylesheet" href="loginStyle.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="header-layer">
+            <h1>Animo<span>Bites</span></h1>
+        </div>
+        <div class="login">
+            <div class="input-field" id = "user">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username">
+            </div>
+            <div class="input-field">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password">
+            </div>
+            <div class="remember-me">
+                <input type="checkbox" id="remember" name="remember">
+                <label for="remember">Remember Me</label>
+            </div>
+            <button class="login-button">Log in</button>
+            <a class="forgot-password" href="">Forgot Password?</a>
+            
+        </div>
+            <a class="home-link" href="#">← Go Back to Home</a>
+    </div>
+
+    <?php
+         
+    ?>
+</body>
+</html>
