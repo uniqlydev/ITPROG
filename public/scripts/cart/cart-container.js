c40c99256d9e5a86e5d3fd4650c75674114c5c2b
Brendan Castillo <bomber8183@gmail.com>
2023-07-24 17:11:40 +0800

    Dynamically adding to cart, updating total does not work yet

--- public/scripts/cart/cart-container.js
+++ public/scripts/cart/cart-container.js
@@ -1,19 +1,61 @@
-function loadMenu(foods) {
-    
-    for (var i = 0; i < foods.length; i++) {
-        console.log(foods[i]);
+// Variables for subtotal and total
+var totl = 0;
+var subtotal = 0;
+
+var mainSelected, sideSelected, drinkSelected;
+
+var mcounter = 0;
+
+
+
+function add(price) {
+    total += price;
+    document.getElementById('total').value = total;
+    total.value = subtotal.toString();
+}
+
+function checkFields(event) {
+    let mainField = document.getElementById('mainm').value;
+    let sideField = document.getElementById('sides').value;
+    let drinkField = document.getElementById('drinks').value;
+
+    if (mainField == "" && sideField == "" && drinkField == "") {
+        alert("Please fill out at least 1 field");
+        event.preventDefault();
     }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+function clearOrder() {
+    document.getElementById('mainm').value = "";
+    document.getElementById('drinks').value = "";
+    document.getElementById('sides').value = "";
 
-    var cartContainer = document.getElementById('cart-container');
-    var mealbutton = document.getElementById('meal-button');
-    var sidesbutton = document.getElementById('sides-button');
-    var drinkbutton = document.getElementById('drink-button');
+    document.getElementById('quantitymain').value = 0;
+    document.getElementById('quantitydrink').value = 0;
+    document.getElementById('quantityside').value = 0;
+
+    document.getElementById('total').value = 0;
+
+    // Enable buttons
+    document.getElementById('meals1').disabled = false;
+    document.getElementById('meals2').disabled = false;
+    document.getElementById('meals3').disabled = false;
+
+    document.getElementById('drinks1').disabled = false;
+    document.getElementById('drinks2').disabled = false;
+    document.getElementById('drinks3').disabled = false;
+
+    document.getElementById('sides1').disabled = false;
+    document.getElementById('sides2').disabled = false;
+    document.getElementById('sides3').disabled = false;
+
+    // Reset variables
+    subtotal = 0;
+    totl = 0;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
 
 
-    loadMenu(foods);
 
-    }
-);
+});
--- public/scripts/home/index.js
+++ public/scripts/home/index.js
@@ -21,30 +21,74 @@ function checkFields(event) {
     }
 }
 
-function clearOrder() {
-
-    subtotal = 0;
-    totl = 0;
-}
-
 
 document.addEventListener('DOMContentLoaded', () => {
-
     // On load alert instructions
     alert("Please select one (1) meal, one (1) drink, and one (1) side dish. You can add more by clicking the + button. However, having multiple different kinds of meal is not allowed.");
 
-    // Get elements
-                // Add an event listener to the parent <ul> element
-document.getElementById("foodlist").addEventListener("click", function(event) {
-    // Check if the click event occurred on an <li> element
-    if (event.target && event.target.tagName === "LI") {
-        // Handle the click event here
-        // Access the clicked <li> element's data using event.target.dataset or other properties
-        console.log("Clicked on:", event.target.querySelector('.label').innerText); // Example: Accessing the food name
+    // Variables for subtotal, total, and other variables (if any)
+    let totals = 0;
+    let discount = 0;
+    let subtotal = 0;
+
+    // <div class="total-container">
+    // <label for="item">Total:</label>
+    // <input type="text" id="total" class="ttl" name="total" required readonly value="0" >
+    // </div>
+    const total = document.getElementById('total');
+
+    // Function to add the selected item to the cart
+    function addToCart(foodName, price) {
+        document.getElementById('mainm').value = foodName;
+        document.getElementById('mainm').hidden = false;
+        document.getElementById('msubtract').hidden = false;
+        document.getElementById('quantitymain').hidden = false;
+        document.getElementById('madd').hidden = false;
+        document.getElementById('quantitymain').value = 1;
+        document.getElementById('quantitymain').disabled = false;
+        document.getElementById('price-main').textContent = price.toFixed(2);
+
+        // Update text of total
+        subtotal += price;
+        total.value = subtotal.toFixed(2);
     }
+
+
+    function addQuantity() {
+        let quantity = document.getElementById('quantitymain');
+        quantity.value = parseInt(quantity.value) + 1;
+    }
+
+    function subtractQuantity() {
+        let quantity = document.getElementById('quantitymain');
+
+        if (parseInt(quantity.value) - 1 < 0) {
+            return;
+        }
+        quantity.value = parseInt(quantity.value) - 1;
+    }
+
+    // Attach event listeners to each li element
+    const lis = document.querySelectorAll('li[id^="m"]');
+    lis.forEach((li, index) => {
+        const button = li.querySelector('.button-menu');
+        const price = parseFloat(li.querySelector('p[id^="price-"]').textContent);
+        const foodName = li.querySelector('.label').textContent;
+
+        button.addEventListener('click', () => {
+            addToCart(foodName, price);
+        });
+    });
+
+    // Add event listener to add button
+    document.getElementById('madd').addEventListener('click', () => {
+        addQuantity();
+    });
+
+    // Add event listener to subtract button
+    document.getElementById('msubtract').addEventListener('click', () => {
+        subtractQuantity();
+    });
 });
 
 
-
-    
-});
