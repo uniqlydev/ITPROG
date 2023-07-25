aeacb16166757f9125913cb064d23b1a73dd68c0
Brendan Castillo <bomber8183@gmail.com>
2023-07-25 17:39:36 +0800

    Dynamic already working but need to apply to all fields

--- public/scripts/home/index.js
+++ public/scripts/home/index.js
@@ -31,6 +31,8 @@ document.addEventListener('DOMContentLoaded', () => {
     let discount = 0;
     let subtotal = 0;
 
+    const totalField = document.getElementById('total');
+
     // <div class="total-container">
     // <label for="item">Total:</label>
     // <input type="text" id="total" class="ttl" name="total" required readonly value="0" >
@@ -46,33 +48,34 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('madd').hidden = false;
         document.getElementById('quantitymain').value = 1;
         document.getElementById('quantitymain').disabled = false;
-        document.getElementById('price-main').textContent = price.toFixed(2);
+        document.getElementById('mainphp').hidden = false;
+        document.getElementById('mainphp').value = price.toFixed(2);
 
-        // Update text of total
+
         subtotal += price;
-        total.value = subtotal.toFixed(2);
+        totalField.value = subtotal.toFixed(2);
     }
 
 
-    function addQuantity() {
-        let quantity = document.getElementById('quantitymain');
-        quantity.value = parseInt(quantity.value) + 1;
+    function addQuantity(qtty, price) {
+        qtty.value = parseInt(qtty.value) + 1;
+        subtotal += price;
+        totalField.value = subtotal.toFixed(2);
     }
 
-    function subtractQuantity() {
-        let quantity = document.getElementById('quantitymain');
-
-        if (parseInt(quantity.value) - 1 < 0) {
-            return;
+    function subtractQuantity(qtty, price) {
+        if (qtty.value > 0) {
+            qtty.value = parseInt(qtty.value) - 1;
+            subtotal -= price;
+            totalField.value = subtotal.toFixed(2);
         }
-        quantity.value = parseInt(quantity.value) - 1;
     }
 
     // Attach event listeners to each li element
     const lis = document.querySelectorAll('li[id^="m"]');
     lis.forEach((li, index) => {
         const button = li.querySelector('.button-menu');
-        const price = parseFloat(li.querySelector('p[id^="price-"]').textContent);
+        const price = parseFloat(li.querySelector('p[id^="price-"]').textContent.trim().replace('PHP', ''));
         const foodName = li.querySelector('.label').textContent;
 
         button.addEventListener('click', () => {
@@ -82,12 +85,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Add event listener to add button
     document.getElementById('madd').addEventListener('click', () => {
-        addQuantity();
+        addQuantity(document.getElementById('quantitymain'), parseFloat(document.getElementById('mainphp').value));
     });
 
     // Add event listener to subtract button
     document.getElementById('msubtract').addEventListener('click', () => {
-        subtractQuantity();
+        subtractQuantity(document.getElementById('quantitymain'), parseFloat(document.getElementById('mainphp').value));
     });
 });
 
