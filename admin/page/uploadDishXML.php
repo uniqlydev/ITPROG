b6a04bf904f53e9195e0d1913de3f81d49869f2d
Matthew Wassmer <110731303+Maccheww@users.noreply.github.com>
2023-08-04 22:47:07 +0800

    Fixed design for Phase 2 (INC)

--- admin/page/uploadDishXML.php
+++ admin/page/uploadDishXML.php
@@ -9,20 +9,32 @@
 <head>
     <meta charset="UTF-8" />
     <title>Add Dishes via XML</title>
-    <link rel="stylesheet" href="upDishXMLStyle.css">
+    <link rel="stylesheet" href="uploadDishXMLStyle.css">
 </head>
 <body>
-    <div class="Menu-container">
+    <div class="main-menu">
         <div class="header-layer">
             <h1>Animo<span>Bites</span></h1>
         </div>
-        <div class = "menu">
+        <div class="menu">
+            <button onclick="window.location.href = 'addDish.php';">Add Dishes</button><br>
+            <button onclick="window.location.href = 'editDish.php';">Edit Dishes</button><br>
+            <button onclick="window.location.href = 'deleteDish.php';">Delete Dishes</button><br>
+            <button onclick="window.location.href = 'addCombo.php';">Add Combo</button><br>
+            <button onclick="window.location.href = 'generateReport.php';">Generate Report</button><br>
+                
+             <button onclick="window.location.href = 'logoutPage.php';">Logout</button>
+        </div>
+    </div>
+    <div class="Menu-container">
+        <div class = "menu1">
             <h2>Add Dishes via XML</h2>
-            <div class = "Submenu">
+            <div class="Submenu">
                 <form method="post" action="uploadDishXMLConfirm.php" enctype="multipart/form-data">
                     <input type="file" name="file" id="file" accept=".xml" required>
                     <br>
-                   <button type="submit" value="Upload XML File" name="uploadBtn">Upload XML File </button>
+                    <button type="submit" value="Upload XML File" name="uploadBtn">Upload XML File </button><br />
+                    <button onclick="window.location.href = 'addDish.php';">Back</button>
                 </form>
             </div>
         </div>
--- admin/page/uploadDishXMLConfirm.php
+++ admin/page/uploadDishXMLConfirm.php
@@ -10,71 +10,73 @@
 <head>
     <meta charset="UTF-8" />
     <title>Add Dishes via XML</title>
-    <link rel="stylesheet" href="DishXMLConStyle.css">
+    <link rel="stylesheet" href="uploadDishXMLConfirmStyle.css">
 </head>
     <body>
-        <div class="Menu-container">
+        <div class="main-menu">
             <div class="header-layer">
                 <h1>Animo<span>Bites</span></h1>
             </div>
-        <div class = "menu">
-            <?php
-                if (isset($_POST["uploadBtn"])) {
-                    echo "<h2>Upload Dishes via XML Confirmation</h2>";
+            <div class="menu">
+                <button onclick="window.location.href = 'addDish.php';">Add Dishes</button><br>
+                <button onclick="window.location.href = 'editDish.php';">Edit Dishes</button><br>
+                <button onclick="window.location.href = 'deleteDish.php';">Delete Dishes</button><br>
+                <button onclick="window.location.href = 'addCombo.php';">Add Combo</button><br>
+                <button onclick="window.location.href = 'generateReport.php';">Generate Report</button><br>
                     
-                    $target_dir = "uploads/";
-                    $target_file = $target_dir . basename($_FILES["file"]["name"]);
-
-                    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-                        echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
-                        echo "<br /><br />";
-                        echo "<a href = 'adminPage.php'>Back</a>";// it doesnt do anything when pressed
+                <button onclick="window.location.href = 'logoutPage.php';">Logout</button>
+            </div>
+        </div>
+        <div class="Menu-container">
+            <div class = "menu1">
+                <?php
+                    if (isset($_POST["uploadBtn"])) {
+                        echo "<h2>Upload Dishes via XML Confirmation</h2>";
+                        
+                        // Move file to uploads folder
+                        $target_dir = "uploads/";
+                        $target_file = $target_dir . basename($_FILES["file"]["name"]);
+                        $uploadOk = 1;
+                        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
                         $success = true;
-                        echo "<br /><br />";
-                    } else {
-                        echo "Sorry, there was an error uploading your file.";
-                        echo "<br /><br />";
-                        echo "<a href = 'uploadDishXML.php';'>Back</a>";
-                        $success = false;
-                        echo "<br /><br />";
+
+                        rename($target_file, "uploads/dishes.xml");
                     }
 
-                    rename($target_file, "uploads/dishes.xml");
-                }
+                    // View XML data in table format
+                    if ($success) {
+                        echo "<form method='post' action='uploadDishXMLDatabase.php'>";
+                            $dishes = simplexml_load_file("uploads/dishes.xml") or die("Error: Cannot create object");
 
-                // View XML data in table format
-                if ($success) {
-                    echo "<form method='post' action='uploadDishXMLDatabase.php'>";
-                        $dishes = simplexml_load_file("uploads/dishes.xml") or die("Error: Cannot create object");
-
-                        echo "<table border='1'>";
-                        echo "<tr style='color:white'>";
-                        echo "<th>Dish ID</th>";
-                        echo "<th>Dish Name</th>";
-                        echo "<th>Dish Price</th>";
-                        echo "<th>Dish Image</th>";
-                        echo "<th>Dish Description</th>";
-                        echo "<th>Dish Protein</th>";
-                        echo "<th>Dish Type</th>";
-                        echo "</tr>";
-                    
-                        foreach ($dishes->dish as $dish) {
+                            echo "<table border='1'>";
                             echo "<tr style='color:white'>";
-                            echo "<td>" . $dish->id . "</td>";
-                            echo "<td>" . $dish->name . "</td>";
-                            echo "<td>" . $dish->price . "</td>";
-                            echo "<td>" . $dish->image . "</td>";
-                            echo "<td>" . $dish->description . "</td>";
-                            echo "<td>" . $dish->protein . "</td>";
-                            echo "<td>" . $dish->type . "</td>";
+                            echo "<th>Dish ID</th>";
+                            echo "<th>Dish Name</th>";
+                            echo "<th>Dish Price</th>";
+                            echo "<th>Dish Image</th>";
+                            echo "<th>Dish Description</th>";
+                            echo "<th>Dish Protein</th>";
+                            echo "<th>Dish Type</th>";
                             echo "</tr>";
-                        }
-                        echo "</table>";
-                        echo "<br /><br />";
-                        echo "<button type='submit' name='uploadBtn' value='Upload to Database' >Upload to Database </button>";
-                    echo "</form>";
-                }
-            ?>
+                        
+                            foreach ($dishes->dish as $dish) {
+                                echo "<tr style='color:white'>";
+                                echo "<td>" . $dish->id . "</td>";
+                                echo "<td>" . $dish->name . "</td>";
+                                echo "<td>" . $dish->price . "</td>";
+                                echo "<td>" . $dish->image . "</td>";
+                                echo "<td>" . $dish->description . "</td>";
+                                echo "<td>" . $dish->protein . "</td>";
+                                echo "<td>" . $dish->type . "</td>";
+                                echo "</tr>";
+                            }
+                            echo "</table>";
+                            echo "<br /><br />";
+                            echo "<button type='submit' name='uploadBtn' value='Upload to Database' >Upload to Database </button>";
+                        echo "</form>";
+                    }
+                ?>
+            </div>
         </div>
     </body>
 </html>
