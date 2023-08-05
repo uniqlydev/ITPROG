aa8b4a1317334cf6820f6d882d65fc828a80d8f0
Brendan Castillo <bomber8183@gmail.com>
2023-08-05 18:18:21 +0800

    revered xml

--- admin/page/generateReportXML.php
+++ admin/page/generateReportXML.php
@@ -39,16 +39,12 @@
                     $totalDiscount = $_POST["totalDiscount"];
                     $fileName = $_POST["fileName"];
 
-                    $fileName = "../reports/" . $_POST["fileName"];
-
-                    if (strpos($fileName, '.xml') === false) {
-                        $fileName = $fileName . ".xml";
+                    if (strpos($fileName, '.xml') !== false) {
+                        // echo 'true';
                     } else {
                         $fileName = $fileName . ".xml";
                     }
 
-                    $fileName = "../reports/" . $_POST["fileName"];
-                    
                     if (isset($_POST["generateReportBtn"])) {
                         if (file_exists($fileName)) {
                             echo "<h1>File already exists!</h1>";
@@ -62,14 +58,13 @@
                             $report->addChild("totalAmount", $totalAmount);
                             $report->addChild("totalDiscount", $totalDiscount);
                             $report->addChild("endDate", $endDate);
-                    
+
                             file_put_contents($fileName, $reports->asXML());
-                    
+
                             echo "<h1>Report XML file generated successfully!</h1>";
                             echo "<a href='generateReport.php'>Back</a>";
                         }
                     }
-                    
                 ?>
             </div>
         </div>
